03242caacde6da8984cc830af134c01b0883f9fd
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-12-09 01:37:38 +0700

    replace belajar with sharing on homepage

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,9 +1,13 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <nuxt-link class="flex justify-center pt-8 sm:pt-0" :to="{name:'index'}">
+      <nuxt-link
+        class="flex justify-center pt-8 sm:pt-0"
+        :to="{ name: 'index' }"
+      >
         <img class="w-20" src="/img/ngodingmanusiawi-logo.svg" />
       </nuxt-link>
       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
@@ -14,7 +18,8 @@
           Ngoding Untuk Bermanfaat Seluas-Luasnya.
         </p>
         <p class="mt-4 pt-4 text-gray-800 border-t border-dashed text-center">
-          Tempat belajar ngoding untuk bermanfaat seluas-luasnya.
+          Tempat <span class="italic">sharing</span> ngoding untuk bermanfaat
+          seluas-luasnya.
         </p>
       </div>
     </div>
