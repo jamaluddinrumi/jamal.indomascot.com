aee78c5f01f66974f5878f4fe0175976364e1fc1
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-12-09 00:32:16 +0700

    set default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'ngodingmanusiawi',
+    title: 'Ngoding Manusiawi - Ngoding Untuk Bermanfaat Seluas-Luasnya.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
