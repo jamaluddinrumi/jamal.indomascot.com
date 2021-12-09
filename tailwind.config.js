dd1b35597922705a32f257983c913d9432165c0d
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-12-09 16:40:21 +0700

    adding tailwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,41 @@
 module.exports = {
+  important: true,
   mode: "jit",
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  // Active dark mode on class basis
+  darkMode: "class",
+  i18n: {
+    locales: ["en-US"],
+    defaultLocale: "en-US",
+  },
   theme: {
-    extend: {},
+    extend: {
+      spacing: { 128: "32rem" },
+      colors: {
+        "rakarsa": "#206ee9",
+        "whatsapp-teal-green": "#128C7E",
+        "whatsapp-teal-green-dark": "#075E54",
+        "whatsapp-light-green": "#25D366",
+        "whatsapp-blue": "#34B7F1",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ["checked"],
+      borderColor: ["checked"],
+      inset: ["checked"],
+      zIndex: ["hover", "active"],
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
+  future: {
+    purgeLayersByDefault: true,
+  },
 };
