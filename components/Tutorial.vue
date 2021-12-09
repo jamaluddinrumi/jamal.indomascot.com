e9b2b5f50498dbb323386c6308752b789a56cdc3
Jamaluddin Rumi <jamal.rumi@icloud.com>
2021-12-09 16:40:33 +0700

    beautify homepage

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,24 +1,16 @@
 <!-- Please remove this file from your project -->
 <template>
   <div
-    class="
-      relative
-      flex
-      items-top
-      justify-center
-      min-h-screen
-      bg-gray-100
-      sm:items-center sm:pt-0
-    "
+    class="relative flex items-center justify-center min-h-screen bg-gray-100"
   >
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+    <div class="max-w-4xl mx-auto px-4 md:px-6">
       <nuxt-link
         class="flex justify-center pt-8 sm:pt-0"
         :to="{ name: 'index' }"
       >
         <img class="w-20" src="/img/ngodingmanusiawi-logo.svg" />
       </nuxt-link>
-      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+      <div class="mt-4 bg-white overflow-hidden shadow sm:rounded-lg p-6">
         <h1
           class="
             text-2xl
@@ -26,35 +18,26 @@
             font-semibold
             text-center text-transparent
             bg-clip-text bg-gradient-to-r
-            from-green-400
-            to-blue-600
+            from-green-600
+            to-blue-900
           "
         >
           Ngoding Manusiawi
         </h1>
         <h2
           class="
-            mt-3
-            text-gray-600 text-center text-transparent
+            mt-1
+            text-base
+            leading-7
+            font-semibold
+            text-center text-transparent
             bg-clip-text bg-gradient-to-r
-            from-pink-600
-            to-yellow-900
+            from-purple-600
+            to-indigo-900
           "
         >
           Ngoding Untuk Bermanfaat Seluas-Luasnya.
         </h2>
-        <p
-          class="
-            mt-4
-            text-gray-800 text-center text-transparent
-            bg-clip-text bg-gradient-to-r
-            from-blue-600
-            to-indigo-900
-          "
-        >
-          Tempat <span class="italic">sharing</span> ngoding untuk bermanfaat
-          seluas-luasnya.
-        </p>
       </div>
     </div>
   </div>
